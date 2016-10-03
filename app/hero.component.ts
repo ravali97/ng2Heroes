6795b7ab9d397fb97a229dfb739f1d48926d8535
ravali97 <ravali90@yahoo.com>
2016-10-03 12:26:17 -0700

    Created a view from hero component to view details about hero

--- app/hero.component.ts
+++ app/hero.component.ts
@@ -1,67 +1,13 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
 
 @Component({
 
 	selector: 'my-heroes',
-	template:
-	'<ul class="heroes">' +
-	'<li *ngFor="let hero of heroes" [class.selected]="hero===selectedHero" (click)="onSelect(hero)">' +
-	'<span class="badge">{{hero.id}}</span> {{hero.name}} </li>' +
-	'</ul>' +
-
-	'<my-hero-detail [hero]="selectedHero"></my-hero-detail>'
-	,
-	styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .heroes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 10px;
-    width: 15em;
-  }
-  .heroes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .35em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`]
+	templateUrl: '/app/hero.component.html',
+	styleUrls: ['/app/hero.component.css']
 })
 
 export class HeroComponent implements OnInit {
@@ -71,7 +17,8 @@ export class HeroComponent implements OnInit {
 	selectedHero: Hero;
 
 
-	constructor(private heroService: HeroService) {
+	constructor(private heroService: HeroService,
+				private router: Router) {
 	}
 
 	ngOnInit(): void {
@@ -88,6 +35,12 @@ export class HeroComponent implements OnInit {
 	onSelect(hero: Hero) {
 
 		this.selectedHero = hero;
+	}
+
+	gotoDetail(hero: Hero) {
+
+		let link = ['/detail', hero.id];
+		this.router.navigate(link);
 	}
 
 }
